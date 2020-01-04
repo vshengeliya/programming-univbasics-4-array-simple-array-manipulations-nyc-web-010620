2049448c353c3dc2a020612de85e7ccc00a18a67
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 04:25:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,56 +2,7 @@
     array.push(string)
   end
     
-#   def bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-#     bouroughs_in_nyc.unshift("Staten Island")
-#     p bouroughs_in_nyc
-#   end
-  
-#   def continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-#     continents.pop
-#   end
-  
-#   def pop_with_args = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-#   pop_with_args.pop(2)
-#   end 
 
-
-# def my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-# my_favorite_cities.shift
-# end
-
-
-# def shift_with_arg=["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-#   shift_with_arg.shift(2)
-# end
-
-
-# def my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-#   more_favs = ["sports cars", "flatiron school"]
-#   my_favorite_things.concat(more_favs)
-# end
-  
-#   def using_isert = ["one", "two", "three", "four"]
-#   using_isert.insert(4, "five")
-# end
-
-
-# def using_uniq = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"] 
-#   using_uniq.uniq 
-# end
-
-# def using_flatten = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-#   using_flatten.flatten
-# end
-
-
-# def using_delete = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-# using_delete.delete("Steven")
-# end
-
-# def using_delete_at =["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-# using_delete.delete_at(2)
-# end
 
 
   
