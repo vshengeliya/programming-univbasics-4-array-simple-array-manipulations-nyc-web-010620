d1bfcfcdc5b9c4b0e4a91d45a13e1d218363fd6e
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 04:24:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,5 @@
   def colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
     colors_in_the_rainbow.push("violet")
-    p colors_in_the_rainbow
     end
     
 #   def bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
