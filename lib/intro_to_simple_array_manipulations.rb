9a96b94249df4c692af13a23a8115f154ca83f79
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 04:40:33 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,60 +1,50 @@
-  def colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    colors_in_the_rainbow.push("violet")
-    p colors_in_the_rainbow
-    end
-    
-     bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    bouroughs_in_nyc.unshift("Staten Island")
-    p bouroughs_in_nyc
+  def using_push(array, string)
+    array.push(string)
   end
   
-  def continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-    continents.pop
+  def using_unshift(array, string)
+    array.unshift(string)
   end
   
-  def pop_with_args = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  pop_with_args.pop(2)
+  def using_pop(array)
+    array.pop
+  end
+  
+  def pop_with_args(array)
+    array.pop(2)
   end 
-
-
-def my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-my_favorite_cities.shift
-end
-
-
-def shift_with_arg=["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  shift_with_arg.shift(2)
-end
-
-
-def my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  my_favorite_things.concat(more_favs)
-end
   
-  def using_isert = ["one", "two", "three", "four"]
-  using_isert.insert(4, "five")
+  def using_shift(array)
+    array.shift
+  end
+    
+  def shift_with_args(array)
+    array.shift(2)
+  end
+  
+  def using_concat(array1, array2)
+    array1.concat(array2)
+  end
+
+def using_insert(array, element)
+  array.insert(4, element)
 end
 
-
-def using_uniq = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"] 
-  using_uniq.uniq 
+def using_uniq(array)
+  array.uniq
 end
 
-def using_flatten = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-  using_flatten.flatten
+def using_flatten(array)
+  array.flatten
 end
 
-
-def using_delete = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-using_delete.delete("Steven")
+def using_delete(array, string)
+  array.delete(string)
 end
 
-def using_delete_at =["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-using_delete.delete_at(2)
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
-
-
   
   
 
